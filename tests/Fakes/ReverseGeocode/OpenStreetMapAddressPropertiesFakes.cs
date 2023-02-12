df3de1f61d6a6d9324829e1ca03b63e8af21830b
Alp Coker <ac@alpcoker.com>
2023-02-12 21:25:59 +0100

    fix: not require vendor reverse geocode param
    fixes #9

--- tests/Fakes/ReverseGeocode/OpenStreetMapAddressPropertiesFakes.cs
+++ tests/Fakes/ReverseGeocode/OpenStreetMapAddressPropertiesFakes.cs
@@ -10,4 +10,10 @@ public static class OpenStreetMapAddressPropertiesFakes
 		var requestedAddressProperties = typeof(OpenStreetMapAddress).GetProperties().Where(w => properties.Contains(w.Name)).ToList();
 		return requestedAddressProperties;
 	}
+
+	public static List<string> Valid()
+	{
+		var validPropertyInfos = ValidPropertyInfos();
+		return validPropertyInfos.Select(s => s.Name.ToString()).ToList();
+	}
 }
--- tests/IntegrationTests/PackageTests/FluentValidation/AddressOptionsFluentValidationTests.cs
+++ tests/IntegrationTests/PackageTests/FluentValidation/AddressOptionsFluentValidationTests.cs
@@ -2,6 +2,8 @@
 
 public class AddressOptionsFluentValidationTests : BaseFluentValidationTests<AddressOptions, AddressOptionsValidator>
 {
+	#region Invalid
+
 	[Fact]
 	public void Null_InputFolderPath_Should_Give_NotNullValidator_Error()
 	{
@@ -12,16 +14,16 @@ public class AddressOptionsFluentValidationTests : BaseFluentValidationTests<Add
 	[Fact]
 	public void Using_InputPath_Without_Jpg_Or_Jpeg_Extension_Should_Give_RegularExpressionValidator_And_Verify_Error_Message()
 	{
-		var options = new AddressOptions("report.png", ReverseGeocodeProviderFakes.Valid(), AddressListTypeFakes.Valid());
+		var options = new AddressOptions(FileNameFakes.InvalidInputPhotoPath, ReverseGeocodeProviderFakes.Valid(), AddressListTypeFakes.Valid());
 		CheckPropertyRegularExpression(options, nameof(AddressOptions.InputPath), $"{nameof(AddressOptions.InputPath)} should have .jpg or .jpeg extension");
 	}
 
-	#region ReverseGeocode Providers
+	#region AddressListType SelectedProperties Require Additional Vendor Specific Properties
 
 	[Fact]
-	public void When_Using_BigDataCloud_Not_Using_BigDataCloudAdminLevels_Should_Give_NullValidator_And_Verify_Error_Message()
+	public void When_Using_BigDataCloud_With_AddressListType_As_SelectedProperties_Not_Using_BigDataCloudAdminLevels_Should_Give_NullValidator_And_Verify_Error_Message()
 	{
-		var options = AddressOptionsFakes.WithReverseGeocodeService(ReverseGeocodeProvider.BigDataCloud);
+		var options = AddressOptionsFakes.WithReverseGeocodeServiceAndAddressListType(ReverseGeocodeProvider.BigDataCloud, AddressListType.SelectedProperties);
 		CheckPropertyNotEmpty(options, nameof(AddressOptions.BigDataCloudAdminLevels), MustUseMessage(nameof(AddressOptions.BigDataCloudAdminLevels), nameof(ReverseGeocodeProvider.BigDataCloud),
 			"bigdatacloud-levels", 'v'));
 	}
@@ -29,19 +31,61 @@ public class AddressOptionsFluentValidationTests : BaseFluentValidationTests<Add
 	[Theory]
 	[InlineData(ReverseGeocodeProvider.OpenStreetMapFoundation)]
 	[InlineData(ReverseGeocodeProvider.LocationIq)]
-	public void When_Using_OpenStreetMap_Not_Using_OpenStreetMapProperties_Should_Give_NullValidator_And_Verify_Error_Message(ReverseGeocodeProvider reverseGeocodeProvider)
+	public void When_Using_OpenStreetMap_With_AddressListType_As_SelectedProperties_Not_Using_OpenStreetMapProperties_Should_Give_NullValidator_And_Verify_Error_Message(ReverseGeocodeProvider reverseGeocodeProvider)
 	{
-		var options = AddressOptionsFakes.WithReverseGeocodeService(reverseGeocodeProvider);
+		var options = AddressOptionsFakes.WithReverseGeocodeServiceAndAddressListType(reverseGeocodeProvider, AddressListType.SelectedProperties);
 		CheckPropertyNotEmpty(options, nameof(AddressOptions.OpenStreetMapProperties),
 			MustUseMessage(nameof(AddressOptions.OpenStreetMapProperties), reverseGeocodeProvider.ToString(), "openstreetmap-properties", 'r'));
 	}
 
 	[Fact]
-	public void When_Using_GoogleMaps_Not_Using_GoogleMapsAddressTypes_Should_Give_NullValidator_And_Verify_Error_Message()
+	public void When_Using_GoogleMaps_With_AddressListType_As_SelectedProperties_Not_Using_GoogleMapsAddressTypes_Should_Give_NullValidator_And_Verify_Error_Message()
 	{
-		var options = AddressOptionsFakes.WithReverseGeocodeService(ReverseGeocodeProvider.GoogleMaps);
+		var options = AddressOptionsFakes.WithReverseGeocodeServiceAndAddressListType(ReverseGeocodeProvider.GoogleMaps, AddressListType.SelectedProperties);
 		CheckPropertyNotEmpty(options, nameof(AddressOptions.GoogleMapsAddressTypes), MustUseMessage(nameof(AddressOptions.GoogleMapsAddressTypes), nameof(ReverseGeocodeProvider.GoogleMaps),
 			"googlemaps-types", 'm'));
+	}
+
+	#endregion
+
+	#endregion
+
+	#region Valid
+
+	public static TheoryData<AddressListType> ValidOptionsThatRequireNoAdditionalReverseGeocodeParameter = new()
+	{
+		AddressListType.FullResponse, AddressListType.AllAvailableProperties
+	};
+
+	[Theory]
+	[MemberData(nameof(ValidOptionsThatRequireNoAdditionalReverseGeocodeParameter))]
+	public void Valid_AddressOptions_With_No_Additional_ReverseGeocode_Parameter_Should_Have_No_Error(AddressListType addressListType)
+	{
+		var options = new AddressOptions(FileNameFakes.ValidInputPhotoPath, ReverseGeocodeProviderFakes.Valid(), addressListType);
+		ValidationShouldHaveNoError(options);
+	}
+
+	[Fact]
+	public void Valid_AddressOptions_For_GoogleMaps_With_AddressListType_As_SelectedProperties()
+	{
+		var options = new AddressOptions(FileNameFakes.ValidInputPhotoPath, ReverseGeocodeProvider.GoogleMaps, AddressListType.SelectedProperties, googleMapsAddressTypes: GoogleMapsPropertiesFakes.Valid());
+		ValidationShouldHaveNoError(options);
+	}
+
+	[Fact]
+	public void Valid_AddressOptions_For_BigDataCloud_With_AddressListType_As_SelectedProperties()
+	{
+		var options = new AddressOptions(FileNameFakes.ValidInputPhotoPath, ReverseGeocodeProvider.BigDataCloud, AddressListType.SelectedProperties, bigDataCloudAdminLevels: BigDataCloudAdminLevelsFakes.Valid());
+		ValidationShouldHaveNoError(options);
+	}
+
+	[Theory]
+	[InlineData(ReverseGeocodeProvider.OpenStreetMapFoundation)]
+	[InlineData(ReverseGeocodeProvider.LocationIq)]
+	public void Valid_AddressOptions_For_OpenStreetMap_With_AddressListType_As_SelectedProperties(ReverseGeocodeProvider reverseGeocodeProvider)
+	{
+		var options = new AddressOptions(FileNameFakes.ValidInputPhotoPath, reverseGeocodeProvider, AddressListType.SelectedProperties, openStreetMapProperties: OpenStreetMapAddressPropertiesFakes.Valid());
+		ValidationShouldHaveNoError(options);
 	}
 
 	#endregion
