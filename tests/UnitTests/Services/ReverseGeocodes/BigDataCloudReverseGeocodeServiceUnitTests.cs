371ff893277f154c6ced7838a12f12ff7c136efd
Alp Coker <ac@alpcoker.com>
2023-11-16 14:30:58 +0100

    fix: BigDataCloud possible multiple admin level
    fix #27

--- tests/UnitTests/Services/ReverseGeocodes/BigDataCloudReverseGeocodeServiceUnitTests.cs
+++ tests/UnitTests/Services/ReverseGeocodes/BigDataCloudReverseGeocodeServiceUnitTests.cs
@@ -1,3 +1,5 @@
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
+
 namespace PhotoCli.Tests.UnitTests.Services.ReverseGeocodes;
 
 public class BigDataCloudReverseGeocodeServiceUnitTests
@@ -14,7 +16,7 @@ public class BigDataCloudReverseGeocodeServiceUnitTests
 	[MemberData(nameof(WithGivenOptionsReverseGeocodeResultShouldBeGivenExpectedAddressData))]
 	public async Task With_Given_Options_ReverseGeocode_Result_Should_Be_Given_Expected_Address_Data(List<int> adminLevels, List<string> expectedAddresses)
 	{
-		var sut = MockServiceWithValidResponse();
+		var sut = MockGeocodeService(BigDataCloudReverseGeocodeResponseFakes.Ankara());
 		var actualReverseGeocode = await sut.Get(CoordinateFakes.Ankara(), "tr", adminLevels);
 		actualReverseGeocode.Should().BeEquivalentTo(expectedAddresses);
 	}
@@ -31,7 +33,7 @@ public class BigDataCloudReverseGeocodeServiceUnitTests
 	[Fact]
 	public async Task Response_Serialization_Verify()
 	{
-		var sut = MockServiceWithValidResponse();
+		var sut = MockGeocodeService(BigDataCloudReverseGeocodeResponseFakes.Ankara());
 		var bigDataCloudResponse = await sut.SerializeFullResponse(CoordinateFakes.Ankara(), "tr");
 		bigDataCloudResponse.Verify();
 	}
@@ -52,14 +54,28 @@ public class BigDataCloudReverseGeocodeServiceUnitTests
 	[MemberData(nameof(ExpectedAvailableReverseGeocodes))]
 	public async Task AllAvailableReverseGeocodes_Equivalent_To_Expected(Dictionary<string, object> expectedAllAvailableReverseGeocodes)
 	{
-		var sut = MockServiceWithValidResponse();
+		var sut = MockGeocodeService(BigDataCloudReverseGeocodeResponseFakes.Ankara());
 		var actualAllAvailableReverseGeocodes = await sut.AllAvailableReverseGeocodes(CoordinateFakes.Ankara(), "tr");
 		actualAllAvailableReverseGeocodes.Should().BeEquivalentTo(expectedAllAvailableReverseGeocodes);
 	}
 
-	private BigDataCloudReverseGeocodeService MockServiceWithValidResponse()
+	[Fact]
+	public async Task AllAvailableReverseGeocodes_MultipleAdminLevelResponseFromService_ShouldLogWarningMessageOfInconsistentData()
 	{
-		var mockHttpClient = MockHttpClient.WithResponse(BigDataCloudReverseGeocodeResponseFakes.Ankara());
-		return new BigDataCloudReverseGeocodeService(mockHttpClient, NullLogger<BigDataCloudReverseGeocodeService>.Instance, ApiKeyStoreFakes.BigDataCloudValid());
+		var loggerMock = new Mock<ILogger<BigDataCloudReverseGeocodeService>>();
+		var sut = MockGeocodeService(BigDataCloudReverseGeocodeResponseFakes.MultipleAdminLevel(), loggerMock.Object);
+		await sut.AllAvailableReverseGeocodes(CoordinateFakes.Valid(), "tr");
+		var logStatements = new[]
+		{
+			"BigDataCloud returned inconsistent/duplicate data. Multiple admin level on 3 found. Used value: First value on level3, duplicate value: Duplicate value on level3"
+		};
+		loggerMock.VerifyAllLogStatementsAtLeastOnce(LogLevel.Warning, logStatements);
+	}
+
+	private static BigDataCloudReverseGeocodeService MockGeocodeService(string response, ILogger<BigDataCloudReverseGeocodeService>? logger = null)
+	{
+		var mockHttpClient = MockHttpClient.WithResponse(response);
+		logger ??= NullLogger<BigDataCloudReverseGeocodeService>.Instance;
+		return new BigDataCloudReverseGeocodeService(mockHttpClient, logger, ApiKeyStoreFakes.BigDataCloudValid());
 	}
 }
